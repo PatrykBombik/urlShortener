59ef1017685f5790471c06d1384359ef6bf5fd25
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-12 00:10:43 +0200

    views done

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener');
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.post('/shortUrls', (req, res) => {
+
+})
 
 app.listen(process.env.PORT || 5000);
