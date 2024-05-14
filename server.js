a07a4e2e91f347cb59fd840a15fcd716def283bc
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-14 16:00:39 +0200

    models done

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import ShortUrl from "./models/shortUrl.js";
+
 const app = express();
 
 mongoose.connect('mongodb://localhost/urlShortener');
 
-
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
 });
 
-app.post('/shortUrls', (req, res) => {
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
 
+    res.redirect('/');
 })
 
 app.listen(process.env.PORT || 5000);
